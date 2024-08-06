b24861d592cb30d32c4931e9986c13c4ff5eab35
{RX_LEGENT_09X} <115865060+rxlegent09x@users.noreply.github.com>
2024-08-06 11:53:49 +0530

    Update util.js

--- help/util.js
+++ help/util.js
@@ -75,6 +75,39 @@ const is_Valid=({name=undefined,gmail=undefined,pass=undefined})=>{
  }
 
 
+const get_Q=()=>{
+
+    const x = Math.floor(Math.random() * 2);
+    if (x == 1) {
+      //arithmetic
+      const num1 = Math.floor(Math.random() * 11);
+      const num2 = Math.floor(Math.random() * 11);
+      const signArray = ["+", "-", "*"];
+      const sign = signArray[Math.floor(Math.random() * 3)];
+      const que = `${num1}${sign}${num2}=?`;
+      const ans = eval(que.slice(0, -2));
+    
+      return ([que, ans]);
+    
+    }
+    
+    else {
+      //greater&Lesser
+      const num1 = Math.floor(Math.random() * 101);
+      const num2 = Math.floor(Math.random() * 101);
+      const num3 = Math.floor(Math.random() * 101);
+      const q = Math.floor(Math.random() * 2);
+      if (q == 1) {
+        //greater than
+        return ([`find biggest ${num1}, ${num2}, ${num3} ?`, Math.max(num1, num2, num3)]);
+      }
+      else {
+        //less than
+        return ([`find smallest ${num1}, ${num2}, ${num3} ?`, Math.min(num1, num2, num3)]);
+      }
+    
+    }
+    }
 
 
 
@@ -85,5 +118,4 @@ const is_Valid=({name=undefined,gmail=undefined,pass=undefined})=>{
 
 
 
-
-module.exports={encryption,decryption,is_Valid};
+module.exports={encryption,decryption,is_Valid,get_Q};
